d67247d44c357e0cfa4a7f684fa4ed9d2c789b88
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-31 00:12:42 +0100

    Shebang fix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Least Recently Used Caching Implementation
 """
 from collections import OrderedDict
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Most Recently Used Caching Implementation.
 """
 from collections import OrderedDict
