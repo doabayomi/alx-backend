b9182c927e3ef92c1f5b15dbdb0d4d4691129f3c
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-31 00:57:18 +0100

    LIFO fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -29,7 +29,7 @@ class LIFOCache(BaseCaching):
 
         self.cache_data[key] = item
         if len(self.cache_data) > self.MAX_ITEMS:
-            last_in_key = self.cache_data.popitem(last=True)
+            last_in_key, _ = self.cache_data.popitem(last=True)
             print(f"DISCARD: {last_in_key}")
 
     def get(self, key):
